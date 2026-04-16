87c0e86e7190c1715539d4ea6724492b368769e0
sude orman <ormansude5@gmail.com>
2026-04-16 14:34:09 +0300

    Create bfs.c

---
+++ bfs.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define V 5
+
+void BFS(int graph[V][V], int start) {
+    int visited[V] = {0};
+    int queue[V], front = 0, rear = 0;
+
+    visited[start] = 1;
+    queue[rear++] = start;
+
+    while (front < rear) {
+        int node = queue[front++];
+        printf("%d ", node);
+
+        for (int i = 0; i < V; i++) {
+            if (graph[node][i] == 1 && !visited[i]) {
+                visited[i] = 1;
+                queue[rear++] = i;
+            }
+        }
+    }
+}
+
+int main() {
+    int graph[V][V] = {
+        {0,1,1,0,0},
+        {1,0,1,1,0},
+        {1,1,0,0,1},
+        {0,1,0,0,1},
+        {0,0,1,1,0}
+    };
+
+    printf("BFS traversal: ");
+    BFS(graph, 0);
+
+    return 0;
+}
